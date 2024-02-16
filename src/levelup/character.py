8e731c9e757a44fdb2670cc6e572132da7108442
MandK <SpectreFE@protonmail.com>
2024-02-16 12:21:30 -0500

    updating character

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -4,10 +4,12 @@ from levelup.map import Map
 
 DEFAULT_CHARACTER_NAME = "Character"
 class Character:
+    # In python, we don't use getters. So no getPosition or getName for this class
     name = ""
     current_position :Position = Position(-100,-100)
     map :Map = Map()
 
+    # Since python doesn't do method overloading, this is how we support a constructor with optional parameters
     def __init__(self, character_name=DEFAULT_CHARACTER_NAME):
         if character_name.strip() == '':
             self.name = DEFAULT_CHARACTER_NAME
