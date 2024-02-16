2f0208022a500dbb32b5af0228e707015291ab30
MandK <SpectreFE@protonmail.com>
2024-02-16 12:25:01 -0500

    update doubles

--- tests/map_double.py
+++ tests/map_double.py
@@ -8,5 +8,4 @@ class MapDouble (Map):
     STUBBED_Y = 4
 
     def calculate_new_position(self, current_position: Position, direction: Direction) -> Position:
-        # What should we return here so our character tests will work in isolation?
-        return None
+        return Position(self.STUBBED_X, self.STUBBED_Y)
