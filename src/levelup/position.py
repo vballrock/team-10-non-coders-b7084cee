ee1f0e113d759ea8f9d83f513d057fa169c9d566
MandK <SpectreFE@protonmail.com>
2024-02-16 12:40:09 -0500

    add position

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -4,16 +4,12 @@ class Position ():
     y = -100
 
     def __init__(self, x: int, y: int):
-         # TODO: implement method here and remove the print statement below
-        print("Position constructor method not yet implemented")
+        self.x = x
+        self.y = y
 
-    # Overriding the equals operator to make sure we are comparing values on positions correctly - prewritten for you
+    # Overriding the equals operator to make sure we are comparing values on positions correctly
     def __eq__(self, obj):
         if self.x == obj.x and self.y == obj.y:
             return True
         else:
             return False
-
-
-
-    
